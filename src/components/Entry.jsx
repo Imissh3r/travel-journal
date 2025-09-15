fb7b680d41f913a42a52f051f8ed242c44a787b1
Sree Prasanth Jayaraj <52457238+Imissh3r@users.noreply.github.com>
2025-09-15 09:42:43 +0000

    Travel Journal - initial commit

---
+++ src/components/Entry.jsx
@@ -0,0 +1,27 @@
+export default function Entry(props) {
+  console.log(props)
+
+  return (
+    <article className="journal-entry">
+      <div className="journal-entry-image-container">
+        <img
+          className="journal-entry-image"
+          src={props.img.src}
+          alt={props.img.alt}
+        />
+      </div>
+      <div className="journal-entry-info">
+        <img
+          src="./src/assets/marker.png"
+          alt="Marker icon"
+          className="marker"
+        />
+        <span className="journal-entry-country">{props.country}</span>
+        <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
+      </div>
+    </article>
+  )
+}
